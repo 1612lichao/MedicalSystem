a0a91317a149ad03c27b0cc5aefe1f80f809e05f
为你狂！ <@WLY>
2019-09-05 14:28:18 +0800

    王龙宇1

--- MedicalSystemManage/MedicalSystemManageApi/Startup.cs
+++ MedicalSystemManage/MedicalSystemManageApi/Startup.cs
@@ -27,6 +27,7 @@ namespace MedicalSystemManageApi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
