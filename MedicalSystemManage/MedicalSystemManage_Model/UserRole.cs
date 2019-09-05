69ff08164e19758b948178533b6f0cad11918bd7
lyq-ha <liuy5974@163.com>
2019-09-05 14:14:15 +0800

    刘月乔MOdel

---
+++ MedicalSystemManage/MedicalSystemManage_Model/UserRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalScheds.Model
+{
+    /// <summary>
+    /// 角色权限关联表
+    /// </summary>
+    public class UserRole
+    {
+        public int Id { get; set; }//Id
+        public int Use_Id { get; set; }//Use_Id
+        public int Rol_Id { get; set; }//Rol_Id
+        public int UserId { get; set; }//用户Id
+        public int Rid { get; set; }//角色Id
+    }
+}
